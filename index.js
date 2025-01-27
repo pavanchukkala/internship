99a9d37406f0f81ec95b1ce1d8fd0f5286c10924
chukkala pavan kumar <150168684+pavanchukkala@users.noreply.github.com>
2025-01-27 22:18:59 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import HomePage from "./internship_platform_home";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <HomePage />
+  </React.StrictMode>,
+  document.getElementById("root") // Ensure index.html has <div id="root"></div>
+);
